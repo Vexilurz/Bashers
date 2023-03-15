02432e74affe28602908e724973ec50470b981d6
Sergei Potekhin <vexilurz@gmail.com>
2023-03-15 23:42:08 +0300

    just force to player

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -2,18 +2,25 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public Rigidbody rb;
+  public Rigidbody rb;
     
-    public float runSpeed = 500f;
-    public float strafeSpeed = 500f;
-    public float jumpForce = 15f;
+  public float runSpeed = 500f;
+  public float strafeSpeed = 500f;
+  public float jumpForce = 15f;
 
-    protected bool strafeLeft = false;
-    protected bool strafeRight = false;
-    protected bool doJump = false;
+  protected bool strafeLeft = false;
+  protected bool strafeRight = false;
+  protected bool doJump = false;
 
-    void Update()
-    {
-        
-    }
+  void Update()
+  {
+    strafeLeft = Input.GetKey("d");
+    strafeRight = Input.GetKey("a");
+    doJump = Input.GetKeyDown("space");
+  }
+
+  private void FixedUpdate()
+  {
+    rb.AddForce(runSpeed * Time.deltaTime, 0, 0);
+  }
 }
