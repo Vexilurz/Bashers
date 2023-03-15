8519dc94b2264ecd00f02df142d78ace72b2fb86
Sergei Potekhin <vexilurz@gmail.com>
2023-03-15 23:29:41 +0300

    some fix

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,16 +1,17 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Rigidbody rb;
+    
+    public float runSpeed = 500f;
+    public float strafeSpeed = 500f;
+    public float jumpForce = 15f;
 
-    // Update is called once per frame
+    protected bool strafeLeft = false;
+    protected bool strafeRight = false;
+    protected bool doJump = false;
+
     void Update()
     {
         
